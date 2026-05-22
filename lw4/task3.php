e671e2f743b01675adce9deead19c35be1d7710d
dmitrij-chizhov <dmitrij.chzhov@ispring.institute>
2026-05-22 21:11:42 +0300

    Fixed task3 in lw4

--- lw4/task3.php
+++ lw4/task3.php
@@ -1,39 +1,135 @@
 <?php
 
-$dayOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-$zodiac = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
-$date = (string)$_POST['date'] . '.';
+function isNumber(string $str) {
+    for ($i = 0; isset($str[$i]); $i++) {
+        if ($str[$i] < "0" || $str[$i] > "9") {
+            return false;
+        }
+    }
+    return true;
+}
+
+function toLower(string $str) {
+    $map = [
+        "А" => "а", "Б" => "б", "В" => "в", "Г" => "г", "Д" => "д", "Е" => "е", "Ё" => "ё",
+        "Ж" => "ж", "З" => "з", "И" => "и", "Й" => "й", "К" => "к", "Л" => "л", "М" => "м",
+        "Н" => "н", "О" => "о", "П" => "п", "Р" => "р", "С" => "с", "Т" => "т", "У" => "у",
+        "Ф" => "ф", "Х" => "х", "Ц" => "ц", "Ч" => "ч", "Ш" => "ш", "Щ" => "щ", "Ъ" => "ъ",
+        "Ы" => "ы", "Ь" => "ь", "Э" => "э", "Ю" => "ю", "Я" => "я",
+        
+        "A" => "a", "B" => "b", "C" => "c", "D" => "d", "E" => "e", "F" => "f", "G" => "g",
+        "H" => "h", "I" => "i", "J" => "j", "K" => "k", "L" => "l", "M" => "m", "N" => "n",
+        "O" => "o", "P" => "p", "Q" => "q", "R" => "r", "S" => "s", "T" => "t", "U" => "u",
+        "V" => "v", "W" => "w", "X" => "x", "Y" => "y", "Z" => "z"
+    ];
+
+    $lower = "";
+
+    for ($i = 0; isset($str[$i]); $i++) {
+        if (isset($str[$i + 1])) {
+            $twoBytes = $str[$i] . $str[$i+1];
+            if (isset($map[$twoBytes])) {
+                $lower .= $map[$twoBytes];
+                $i++; 
+                continue;
+            }
+        }
+
+        if (isset($map[$str[$i]])) {
+            $lower .= $map[$str[$i]];
+        } else {
+            $lower .= $str[$i];
+        }
+    }
+
+    return $lower;
+}
+
+function TXTToMonth(string $strIn) {
+    $str = toLower($strIn);
+    if ($str === "янв" || $str === "jan") {
+        return 1;
+    } elseif ($str === "фев" || $str === "feb") {
+        return 2;
+    } elseif ($str === "мар" || $str === "mar") {
+        return 3;
+    } elseif ($str === "апр" || $str === "apr") {
+        return 4;
+    } elseif ($str === "май" || $str === "may") {
+        return 5;
+    } elseif ($str === "июн" || $str === "jun") {
+        return 6;
+    } elseif ($str === "июл" || $str === "jul") {
+        return 7;
+    } elseif ($str === "авг" || $str === "aug") {
+        return 8;
+    } elseif ($str === "сен" || $str === "sep") {
+        return 9;
+    } elseif ($str === "окт" || $str === "oct") {
+        return 10;
+    } elseif ($str === "ноя" || $str === "nov") {
+        return 11;
+    } elseif ($str === "дек" || $str === "dec") {
+        return 12;
+    } else {
+        return 0;
+    }
+}
+
+$dayOfMonths = [
+    0 => 0,
+    1 => 31,   
+    2 => 29,   
+    3 => 31,   
+    4 => 30,   
+    5 => 31,   
+    6 => 30,   
+    7 => 31,   
+    8 => 31,   
+    9 => 30,   
+    10 => 31,  
+    11 => 30,  
+    12 => 31   
+];
+$zodiac = ["Овен", "Телец", "Близнецы", "Рак", "Лев", "Дева", "Весы", "Скорпион", "Стрелец", "Козерог", "Водолей", "Рыбы"];
+$date = (string)$_POST["date"] . ".";
 $year = 0;
 $month = 0;
 $day = 0;
 $item = "";
 
-for ($i = 0; $i < strlen($date); $i++){
-    if ($date[$i] != '.' || $date[$i] != '-' || $date[$i] != '/') {
+for ($i = 0; isset($date[$i]); $i++) {
+    if ($date[$i] != "." && $date[$i] != "-" && $date[$i] != "/" && $date[$i] != " ") {
         $item = $item . $date[$i];
     }
-    else {
-        if ((int)$item > 31 || ($day && $month)){
-            $year = (int)$item;
-        }
-        else if ($day) {
-            if ((int)$item > 12) {
-                $month = $day;
-                $day = (int)$item;
+    elseif ($item !== "") {
+        if (isNumber($item)) {
+            if ((int)$item > 31 || ($day && $month)) {
+                $year = (int)$item;
+            }
+            elseif ($day) {
+                if ((int)$item > 12) {
+                    $month = $day;
+                    $day = (int)$item;
+                }
+                else {
+                    $month = (int)$item;
+                }
             }
             else {
-                $month = (int)$item;
+                $day = (int)$item;
             }
-        }
+        } 
         else {
-            $day = (int)$item;
+            $month = TXTToMonth($item);
         }
+        
         $item = "";
     }
 }
 
 if ($month == 2) {
-    if ($year % 400 == 0 || ($year % 4 == 0 && $year % 100 != 0)){
+    if ($year % 400 == 0 || ($year % 4 == 0 && $year % 100 != 0)) {
         if ($day > 29) {
             $day = -1;
         }
